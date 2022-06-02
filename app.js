27480ab4f6c4dee3b6e5875603ea8970bbd3bacd
German Ventura <bi-colored@MacBook-Air-de-German.local>
2022-06-02 16:41:23 -0300

    Subo el proyecto

---
+++ app.js
@@ -0,0 +1,10 @@
+
+const express = require('express');
+const app=express();
+const path=require('path');
+
+app.listen(process.env.PORT || 3000, ()=> {console.log("Servidor corriendo en puerto "+process.env.PORT)})
+app.use(express.static('public')); 
+app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,'/views/index.html'))})
+app.get("/login",(req,res)=>{res.sendFile(path.join(__dirname,'/views/login.html'))})
+app.get("/onboarding",(req,res)=>{res.sendFile(path.join(__dirname,'/views/onboarding.html'))})
