d5f4a63439c57ee5de81ba4d8310c74bc0e3530c
germanVentura <103225954+bicolored@users.noreply.github.com>
2022-06-02 17:08:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app=express();
 const path=require('path');
 
-app.listen(process.env.PORT || 3000, ()=> {console.log("Servidor corriendo en puerto "+process.env.PORT)})
+app.listen(process.env.PORT || 3000, ()=> {console.log("Servidor corriendo en puerto 3000")})
 app.use(express.static('public')); 
 app.get("/",(req,res)=>{res.sendFile(path.join(__dirname,'/views/index.html'))})
 app.get("/login",(req,res)=>{res.sendFile(path.join(__dirname,'/views/login.html'))})
